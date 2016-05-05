379e22c5b0eb94e098044a9c0f6d6c1e88f284de
Helen Koike <helen.koike@collabora.co.uk>
2016-05-05 11:04:47 -0300

    Add virtual machine (qemu kvm) scripts
    Thanks to Padovan

---
+++ vm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+QEMU="kvm"
+VDISK="/home/koike/caneca/collabora/qemu-images/ubuntu-14.04.4-desktop-amd64.qcow2"
+
+$QEMU -hda $VDISK \
+	-s \
+	-smp 1 \
+	-nographic \
+	-kernel arch/x86_64/boot/bzImage \
+	-append "root=/dev/sda1 debug console=ttyS0 console=ttyS1 console=tty1 drm.debug=0xff" \
+	-net nic -net user,hostfwd=tcp::5555-:22 \
+	-m 512 $@
