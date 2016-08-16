4f9e0dce5a7f19ef921fba3a7291607715431bb7
Helen Koike <helen.koike@collabora.co.uk>
2016-08-15 23:54:18 -0300

    mod_add2line.sh: add compiler option

--- mod_add2line.sh
+++ mod_add2line.sh
@@ -4,12 +4,13 @@ mod=`find -name $1`
 echo $mod
 func=$2
 offset=$3
+compiler=$4
 
-nm $mod | grep "${func}$" | grep '^[0-9a-f]* [tT]'
-addr=`nm $mod | grep "${func}$" | grep '^[0-9a-f]* [tT]' | awk '{ print "0x"$1 }'`
+${4}nm $mod | grep "${func}$" | grep '^[0-9a-f]* [tT]'
+addr=`${4}nm $mod | grep "${func}$" | grep '^[0-9a-f]* [tT]' | awk '{ print "0x"$1 }'`
 
 for ad in $addr; do
     echo "Symbol found at $ad, applying offset $offset"
     ad=`printf 0x%x $(($ad + $offset))`
-    addr2line -pifae $mod $ad
+    ${4}addr2line -pifae $mod $ad
 done
