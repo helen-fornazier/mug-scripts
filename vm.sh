a7eb4ab3f9faab3ea4137981e72832df9948dbb6
Helen Koike <helen.koike@collabora.com>
2017-05-18 11:23:15 -0300

    vm.sh add loglevel kernel parameter

--- vm.sh
+++ vm.sh
@@ -8,6 +8,6 @@ $QEMU -hda $VDISK \
 	-smp 1 \
 	-nographic \
 	-kernel arch/x86_64/boot/bzImage \
-	-append "root=/dev/sda1 debug console=ttyS0 console=ttyS1 console=tty1 drm.debug=0xff" \
+	-append "root=/dev/sda1 debug console=ttyS0 console=ttyS1 console=tty1 drm.debug=0xff loglevel=8" \
 	-net nic -net user,hostfwd=tcp::5555-:22 \
 	-m 512 $@
